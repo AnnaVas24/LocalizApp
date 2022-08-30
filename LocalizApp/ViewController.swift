f978dbfc0c6437e2efa395feb9f276fef59b4d16
Anna <anna.vasi4ko@icloud.com>
2022-08-30 15:03:53 +0300

    Initial Commit

---
+++ LocalizApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocalizApp
+//
+//  Created by Vasichko Anna on 30.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
